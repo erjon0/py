8863a7e778d6f83d94fb9ebb1989648f7e061953
Student <erjonbenquku@gmail.com>
2025-10-23 16:13:36 +0200

    dsadasdadas

---
+++ module1/index.py
@@ -0,0 +1,29 @@
+#
+# print('hello')
+#
+# x=2
+# f='tren'
+#
+# print(x)
+#
+# print(type(x))
+# print(type(f))
+#
+# print('hello',f)
+#
+# k=3
+#
+# result=k-x
+#
+# print(result)
+
+shoping=[1,1.2,3]
+
+
+shoping.pop(2)
+shoping.remove(1)
+shoping.append('aaaaa')
+shoping.insert(3,'limon')
+
+print(shoping)
+
