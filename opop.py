ef3db70f3101fc6d17c0985cdc12137fa2508fa3
erjon0 <erjonbenquku@gmail.com>
2025-10-28 15:33:39 +0100

    dssa

---
+++ opop.py
@@ -0,0 +1,10 @@
+word=('nig','ddd','rrr')
+
+print(word[2])
+
+person=('daris',13,'non')
+
+name,age,profession=person
+
+print(name,"'s",profession,"profession","his age",age)
+dsadsadasdasdasdasdasdasd
