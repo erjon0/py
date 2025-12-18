ad5ce8e9747ee32c12fcc91c9444449413e6b46f
erjon0 <erjonbenquku@gmail.com>
2025-12-18 16:09:15 +0100

    dssdsaddasdasdassd

---
+++ module13/pandas.py
@@ -0,0 +1,27 @@
+from operator import index
+#
+import pandas as pd
+#
+# products=['apple','bananas','oranges','grapes','pineaples']
+#
+# sales=[15,20,18,9,6]
+#
+# sales_siries=pd.Siries(sales,index=products)
+# print(sales_siries)
+#
+# print(sales_siries['grapes'])
+#
+# total_sales=sales_siries.sum()
+# print(total_sales)
+#
+# best_sell=sales_siries.idxmax()
+# print(best_sell)
+
+data={
+    'name':['alice','bob','charlie']
+    'age':[23,30,22]
+    'city':['new york','berlin','vien']
+}
+
+df=pd.DataFrame(data)
+print(df)
